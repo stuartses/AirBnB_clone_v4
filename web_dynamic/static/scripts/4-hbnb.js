742529b1cc3785c149499c60aacb864cf1ef6c4d
stuartses <stuart.ses@hotmail.com>
2020-05-18 16:36:33 -0500

    Check semistandard

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -1,97 +1,103 @@
+/*
+   Insert places
+   Make the loop and insert html
+*/
+function insertPlaces (place) {
+  for (let i = 0; i < place.length; i++) {
+    let guest = '';
+    if (place[i].max_guest !== 1) guest = 's';
+
+    let bedroom = '';
+    if (place[i].number_rooms !== 1) bedroom = 's';
+
+    let bathroom = '';
+    if (place[i].number_rooms !== 1) bathroom = 's';
+
+    const sectionHtml = '<article>' +
+        '<div class="title_box">' +
+        '<h2>' + place[i].name + '</h2>' +
+        '<div class="price_by_night">' +
+        place[i].price_by_night +
+        '</div>' +
+        '</div>' +
+        '<div class="information">' +
+        '<div class="max_guest">' + place[i].max_guest + ' Guest' + guest +
+        '</div>' +
+        '<div class="number_rooms">' + place[i].number_rooms +
+        ' Bedroom' + bedroom + '</div>' +
+        '<div class="number_bathrooms">' + place[i].number_bathrooms +
+        ' Bathroom' + bathroom + '</div>' +
+        '</div>' +
+        '<div class="user"></div>' +
+        '<div class="description">' + place[i].description + '</div>' +
+        '</acticle>';
+
+    $('section.places').append(sectionHtml);
+  }
+}
+
+/*
+ Onload document
+ Load after DOM
+*/
+
 $(document).ready(() => {
-	if ($('input[type="checkbox"]:checked').length > 0 == true) $('input[type="checkbox"]:checked').prop("checked", false);
+  // reset checkboxes
+  if ($('input[type="checkbox"]:checked').length > 0) {
+    $('input[type="checkbox"]:checked').prop('checked', false);
+  }
 
-	// aqui va el checkcito
-	let check = {}
-	$(document).on('change', 'input[type="checkbox"]', function () {
-		if (!this.checked) delete check[$(this).data('id')]
-		else check[$(this).data('id')] = $(this).data('name')
+  // this is the check. Append amenity to object "check"
+  const check = {};
+  $(document).on('change', 'input[type="checkbox"]', function () {
+    if (!this.checked) delete check[$(this).data('id')];
+    else check[$(this).data('id')] = $(this).data('name');
 
-		let obj = Object.values(check)
-		if (obj < 0) $('div.amenities > h4')
-		$('div.amenities > h4').text(Object.values(check).join(', '))
-	})
+    const obj = Object.values(check);
+    if (obj < 0) $('div.amenities > h4');
+    $('div.amenities > h4').text(Object.values(check).join(', '));
+  });
 
+  // request to check status
+  $.get('http://0.0.0.0:5001/api/v1/status/', function (data, textStatus) {
+    if (data.status === 'OK') {
+      $('div#api_status').toggleClass('not-available available');
+    }
+  })
+    .fail(function () {
+      /* doesn't works with toggle, when load at first time,
+         and port 5001 is down */
+      $('div#api_status').removeClass('available');
+      $('div#api_status').addClass('not-available');
+    });
 
-	// check api status
-	let api = 'http://127.0.0.1:5001/api/v1/status/'
-	$.get(api, (data, code) => {
-		if (code == 'success') {
-			if (data.status != 'OK') $('#api_status').removeClass('available')
-			$('#api_status').addClass('available')
-		}
-	})
+  // get places and insert in html
+  $.ajax({
+    url: 'http://0.0.0.0:5001/api/v1/places_search/',
+    dataType: 'json',
+    type: 'post',
+    contentType: 'application/json',
+    data: JSON.stringify({}),
+    success: function (data, textStatus, jQxhr) {
+      insertPlaces(data); // make the loop and insert html
+    },
+    error: function (jqXhr, textStatus, errorThrown) {
+      console.log(errorThrown);
+    }
+  });
 
-  // append places to the places
-	$.ajax({
-		type: 'POST',
-		url: 'http://127.0.0.1:5001/api/v1/places_search',
-		data: '{}',
-		dataType: 'json',
-		contentType: 'application/json',
-		success: function (data) {
-			for (let i = 0; i < data.length; i++) {
-				let place = data[i];
-				let placeHolder = `
-				<article>
-				<h2>${place.name}</h2>
-				<div class="price_by_night"><p>$ ${place.price_by_night}</p></div>
-				<div class="information">
-				<div class="max_guest">
-				<div class="guest_image">
-				</div>
-				<p>${place.max_guest}</p>
-				</div>
-				<div class="number_rooms"><div class="bed_image"></div><p>${place.number_rooms}</p>
-				</div>
-				<div class="number_bathrooms"><div class="bath_image"></div>
-				<p>${place.number_bathrooms}</p>
-				</div>
-				</div>
-				<div class="description"><p>${place.description}</p></div>
-				</article>
-				`
-				$('.places ').append(placeHolder);
-			}
-		}
-	});
-
-	// when the button is pressed
-	$('.filters > button').click( () => {
+  // when the button is pressed
+  $('.filters > button').click(() => {
     $('.places > article').remove();
-    
-		$.ajax({
-			type: 'POST',
-			url: 'http://127.0.0.1:5001/api/v1/places_search',
-			data: JSON.stringify({'amenities': Object.keys(check)}),
-			dataType: 'json',
-			contentType: 'application/json',
-			success: function (data) {
-				for (let i = 0; i < data.length; i++) {
-					let place = data[i];
-					let placeHolder = `
-					<article>
-					<h2>${place.name}</h2>
-					<div class="price_by_night"><p>$ ${place.price_by_night}</p></div>
-					<div class="information">
-					<div class="max_guest">
-					<div class="guest_image">
-					</div>
-					<p>${place.max_guest}</p>
-					</div>
-					<div class="number_rooms"><div class="bed_image"></div><p>${place.number_rooms}</p>
-					</div>
-					<div class="number_bathrooms"><div class="bath_image"></div>
-					<p>${place.number_bathrooms}</p>
-					</div>
-					</div>
-					<div class="description"><p>${place.description}</p></div>
-					</article>
-					`
-					$('.places ').append(placeHolder);
-				}
-			}
-		});
-	})
-
+    $.ajax({
+      type: 'POST',
+      url: 'http://127.0.0.1:5001/api/v1/places_search',
+      data: JSON.stringify({ amenities: Object.keys(check) }),
+      dataType: 'json',
+      contentType: 'application/json',
+      success: function (data) {
+        insertPlaces(data); // make the loop and insert html
+      }
+    });
+  }); // end when the button is pressed
 });
